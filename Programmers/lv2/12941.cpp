a05a0a6614f4dae4d7ecdf2b70cf2469fe200700
ummjevel <mseagle2023@gmail.com>
2022-09-15 20:26:58 +0900

    12941

--- Programmers/lv2/12941.cpp
+++ Programmers/lv2/12941.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-#include<vector>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int solution(vector<int> A, vector<int> B)
 {
     int answer = 0;
+    // sort
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
 
-
-
+    // min * max +
+    for(int i = 0; i < A.size(); i++) {
+        answer += A[i] * B[B.size() - i - 1];
+    }
+    
     return answer;
 }
 
 int main() {
     vector<int> A = {1, 4, 2};
     vector<int> B = {5, 4, 4};
-    solution(A, B);
+    cout << solution(A, B);
     return 0;
 }
